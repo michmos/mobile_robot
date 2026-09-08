a1f31b4cfac4bc2e3ebe79add1d56408690368cb
Michael Moser <Michael.Moser11@web.de>
2026-09-08 16:55:44 +0200

    add: claim interfaces

--- mobile_robot_controller/src/mobile_robot_controller.cpp
+++ mobile_robot_controller/src/mobile_robot_controller.cpp
@@ -24,14 +24,18 @@ controller_interface::CallbackReturn MobileRobotController::on_init() {
 
 controller_interface::InterfaceConfiguration
 MobileRobotController::command_interface_configuration() const {
-  // TODO: implement
-  return controller_interface::InterfaceConfiguration{};
+  // individual = claim only requested interfaces - not all
+  return {controller_interface::interface_configuration_type::INDIVIDUAL,
+          {leftWheelJointName_ + "/" + hardware_interface::HW_IF_VELOCITY,
+           rightWheelJointName_ + "/" + hardware_interface::HW_IF_VELOCITY}};
 }
 
 controller_interface::InterfaceConfiguration
 MobileRobotController::state_interface_configuration() const {
-  // TODO: implement
-  return controller_interface::InterfaceConfiguration{};
+  // individual = claim only requested interfaces - not all
+  return {controller_interface::interface_configuration_type::INDIVIDUAL,
+          {leftWheelJointName_ + "/" + hardware_interface::HW_IF_POSITION,
+           rightWheelJointName_ + "/" + hardware_interface::HW_IF_POSITION}};
 }
 
 controller_interface::CallbackReturn
